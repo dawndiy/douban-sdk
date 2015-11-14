4f1b8d9457656f98e95555a23f89648fd3191af4
DawnDIY <chenglu1990@gmail.com>
2015-11-14 23:00:02 +0800

    change api

--- api.go
+++ api.go
@@ -1,112 +1,248 @@
 package doubanfm
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
-	"strconv"
+	"net/http/cookiejar"
+	httpurl "net/url"
+	"strings"
 )
 
-// 用户登录
-func (fm *DoubanFM) Login(email, password string) (*User, error) {
-	url := fm.host + "j/app/login"
-	params := map[string][]string{
-		"email":    {email},
-		"password": {password},
-		"app_name": {fm.appName},
-		"version":  {strconv.Itoa(fm.version)},
-	}
+// douban url
+const DoubanURL = "http://douban.fm"
 
-	res, err := http.PostForm(url, params)
+var client *http.Client
+
+// init http client
+func init() {
+	jar, _ := cookiejar.New(nil)
+	client = &http.Client{
+		Jar: jar,
+	}
+}
+
+// SetUser set douban account
+func SetUser(user User) {
+	dbcl2 := user.DBCL2
+	u, _ := httpurl.Parse(DoubanURL)
+	cookies := []*http.Cookie{
+		&http.Cookie{Name: "dbcl2", Value: dbcl2},
+	}
+	client.Jar.SetCookies(u, cookies)
+}
+
+// PlayListNew get a new play list
+func PlayListNew(channel string) ([]Song, error) {
+	return PlayList("n", "", "0.0", channel, "")
+}
+
+// PlayListPlaying get a play list when playing
+func PlayListPlaying(sid, channel string) ([]Song, error) {
+	return PlayList("p", sid, "0.0", channel, "128")
+}
+
+// PlayListSkip get a play list by skip current playing
+func PlayListSkip(sid, pt, channel string) ([]Song, error) {
+	return PlayList("s", sid, pt, channel, "128")
+}
+
+// PlayListBan stop play current list and get a new list
+func PlayListBan(sid, pt, channel string) ([]Song, error) {
+	return PlayList("b", sid, pt, channel, "128")
+}
+
+// PlayListEnd mark finished playing a song
+func PlayListEnd(sid, pt, channel string) ([]Song, error) {
+	return PlayList("e", sid, pt, channel, "128")
+}
+
+// PlayListRate mark like this song
+func PlayListRate(sid, pt, channel string) ([]Song, error) {
+	return PlayList("r", sid, pt, channel, "128")
+}
+
+// PlayListUnrate mark dislike this song
+func PlayListUnrate(sid, pt, channel string) ([]Song, error) {
+	return PlayList("u", sid, pt, channel, "128")
+}
+
+// PlayList get play list from douban.fm
+func PlayList(_type, sid, pt, channel, pb string) ([]Song, error) {
+	api := "/j/mine/playlist"
+	params := map[string]interface{}{
+		"type":    _type,
+		"sid":     sid,
+		"pt":      pt,
+		"channel": channel,
+		"pb":      pb,
+		"from":    "mainsite",
+		"r":       fmt.Sprintf("%x", (int64(rand.Float64()*0xEFFFFFFFFF))+0x1000000000),
+	}
+	url := makeURL(api, params)
+	req, _ := http.NewRequest("GET", url, nil)
+	res, err := client.Do(req)
 	if err != nil {
-		return &User{}, err
+		return nil, err
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return &User{}, err
+		return nil, err
 	}
 
-	user := &User{}
-	json.Unmarshal(body, &user)
-	if user.Result != 0 {
-		return &User{}, errors.New(user.ERR)
+	data := struct {
+		Result int    `json:"r"`
+		Songs  []Song `json:"song"`
+	}{}
+	json.Unmarshal(body, &data)
+
+	for _, v := range res.Cookies() {
+		// logged out
+		if v.Name == "dbcl2" && v.Value == "deleted" {
+			return data.Songs, errors.New("logged out")
+		}
+	}
+
+	return data.Songs, nil
+}
+
+// ChannelChange report chanel change
+func ChannelChange(fromCID, toCID, area string) error {
+	api := "/j/change_channel"
+	params := map[string]interface{}{
+		"fcid": fromCID,
+		"tcid": toCID,
+		"area": area,
+	}
+	url := makeURL(api, params)
+	req, _ := http.NewRequest("GET", url, nil)
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	for _, v := range res.Cookies() {
+		// logged out
+		if v.Name == "dbcl2" && v.Value == "deleted" {
+			return errors.New("logged out")
+		}
+	}
+	return nil
+}
+
+// NewCaptcha get captcha id
+func captchaID() (string, error) {
+	api := "/j/new_captcha"
+	params := map[string]interface{}{}
+	url := makeURL(api, params)
+	res, err := client.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	str := string(body)
+	if strings.HasPrefix(str, "\"") && strings.HasSuffix(str, "\"") {
+		str = strings.Replace(str, "\"", "", -1)
+	}
+	return str, err
+}
+
+// Captcha get captcha base64 string and id
+func Captcha() (string, string, error) {
+	captchaID, err := captchaID()
+	if err != nil {
+		return "", "", err
+	}
+	api := "/misc/captcha"
+	params := map[string]interface{}{
+		"size": "m",
+		"id":   captchaID,
+	}
+	imageURL := makeURL(api, params)
+	res, err := client.Get(imageURL)
+	if err != nil {
+		return "", "", err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	str := base64.StdEncoding.EncodeToString(body)
+
+	return str, captchaID, nil
+}
+
+// Login login douban account
+func Login(name, password, captcha, captchaID string) (User, error) {
+	api := "/j/login"
+	url := makeURL(api, map[string]interface{}{})
+
+	params := httpurl.Values{}
+	params.Set("source", "radio")
+	params.Set("alias", name)
+	params.Set("form_password", password)
+	params.Set("captcha_solution", captcha)
+	params.Set("captcha_id", captchaID)
+
+	req, err := http.NewRequest("POST", url, strings.NewReader(params.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	if err != nil {
+		return User{}, err
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return User{}, err
+	}
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	data := struct {
+		Result int    `json:"r"`
+		ErrMsg string `json:"err_msg"`
+		User   User   `json:"user_info"`
+	}{}
+	json.Unmarshal(body, &data)
+
+	if data.Result != 0 {
+		return User{}, errors.New(data.ErrMsg)
+	}
+
+	user := data.User
+
+	for _, v := range res.Cookies() {
+		if v.Name == "dbcl2" {
+			user.DBCL2 = v.Value
+		}
+		if v.Domain == ".douban.fm" && v.RawExpires != "" {
+			user.Expires = int(v.Expires.Unix())
+		}
 	}
 
 	return user, nil
 }
 
-// 频道
-func (fm *DoubanFM) Channels() ([]Channel, error) {
-
-	url := fm.host + "j/app/radio/channels"
-
-	res, err := http.Get(url)
-	if err != nil {
-		return []Channel{}, err
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return []Channel{}, err
-	}
-
-	data := map[string][]Channel{}
-	json.Unmarshal(body, &data)
-	channels := data["channels"]
-
-	return channels, nil
+// Logout douban account
+func Logout() {
+	jar, _ := cookiejar.New(nil)
+	client.Jar = jar
 }
 
-// 歌曲
-func (fm *DoubanFM) Songs(opts ...map[string]string) ([]Song, error) {
-
-	url := fm.host + "j/app/radio/people"
-
-	var opt map[string]string
-
-	if opts == nil {
-		opt = DefaultOptions
-	} else {
-		opt = opts[0]
-		opt["app_name"] = fm.appName
-		opt["version"] = strconv.Itoa(fm.version)
+// makeURL make url with query string
+func makeURL(api string, kwargs map[string]interface{}) string {
+	params := []string{}
+	for k, v := range kwargs {
+		params = append(params, k+"="+fmt.Sprint(v))
 	}
 
-	if len(opt) > 0 {
-		url += "?"
-	}
-	for k, v := range opt {
-		url += k + "=" + v + "&"
+	if len(params) == 0 {
+		return DoubanURL + api
 	}
 
-	res, err := http.Get(url)
-	if err != nil {
-		return []Song{}, err
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return []Song{}, err
-	}
-
-	data := struct {
-		Result     int    `json:"r"`
-		VersionMax int    `json:"version_max"`
-		Songs      []Song `json:"song"`
-		ERR        string `json:"err"`
-	}{}
-	json.Unmarshal(body, &data)
-
-	if data.Result != 0 {
-		return []Song{}, errors.New(data.ERR)
-	}
-
-	songs := data.Songs
-	return songs, nil
-
+	return DoubanURL + api + "?" + strings.Join(params, "&")
 }
--- douban.go
+++ douban.go
@@ -1,23 +1,26 @@
 package doubanfm
 
-type DoubanFM struct {
-	host    string
-	appName string
-	version int
-}
+import ()
 
-// 用户
+// User is Douban account
 type User struct {
-	ID     string `json:"user_id"`
-	ERR    string `json:"err"`
-	Token  string `json:"token"`
-	Expire string `json:"expire"`
-	Result int    `json:"r"`
-	Name   string `json:"user_name"`
-	Email  string `json:"email"`
+	UID        string     `json:"uid"`
+	Name       string     `json:"name"`
+	ID         string     `json:"id"`
+	IsPro      bool       `json:"is_pro"`
+	PlayRecord PlayRecord `json:"play_record"`
+	DBCL2      string
+	Expires    int
 }
 
-// 频道
+// PlayRecord of Douban account
+type PlayRecord struct {
+	Liked  int `json:"liked"`
+	Played int `json:"played"`
+	Banned int `json:"banned"`
+}
+
+// Channel for Douban FM
 type Channel struct {
 	Name   string      `json:"name"`
 	SeqID  int         `json:"seq_id"`
@@ -26,39 +29,22 @@ type Channel struct {
 	NameEN string      `json:"name_en"`
 }
 
-// 歌曲
+// Song from Douban FM
 type Song struct {
-	Album      string  `json:"album"`
-	Picture    string  `json:"picture"`
-	SSID       string  `json:"ssid"`
-	Artist     string  `json:"artist"`
-	URL        string  `json:"url"`
-	Company    string  `json:"company"`
-	Title      string  `json:"title"`
-	RatingAvg  float64 `json:"rating_avg"`
-	Length     int     `json:"length"`
-	SubType    string  `json:"subtype"`
-	PublicTime string  `json:"public_time"`
-	SID        string  `json:"sid"`
-	AID        string  `json:"aid"`
-	SHA256     string  `json:"sha256"`
-	Kbps       string  `json:"kbps"`
-	AlbumTitle string  `json:"albumtitle"`
-	Like       int     `json:"like"`
-}
-
-// 默认参数
-var DefaultOptions = map[string]string{
-	"app_name": "radio_desktop_win",
-	"version":  "100",
-	"type":     "n",
-	"channel":  "0",
-}
-
-func NewDoubanFM() *DoubanFM {
-	return &DoubanFM{
-		host:    "http://www.douban.com/",
-		appName: "radio_desktop_win",
-		version: 100,
-	}
+	Picture    string `json:"picture"`
+	AlbumTitle string `json:"albumtitle"`
+	Like       int    `json:"like"`
+	Album      string `json:"album"`
+	SSID       string `json:"ssid"`
+	Title      string `json:"title"`
+	URL        string `json:"url"`
+	Artist     string `json:"artist"`
+	SubType    string `json:"subtype"`
+	Length     int    `json:"length"`
+	SID        string `json:"sid"`
+	AID        string `json:"aid"`
+	Company    string `json:"company"`
+	PublicTime string `json:"public_time"`
+	SHA256     string `json:"sha256"`
+	Kbps       string `json:"kbps"`
 }
